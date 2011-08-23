795bf95b1292a8c3702a5e86a7d7c0cb9c6e7391
Paul Nakata <paul@pmn.org>
2011-08-23 10:07:16 -0700

    Incorporating feedback from #clojure

--- src/peeranoia/core.clj
+++ src/peeranoia/core.clj
@@ -1,41 +1,48 @@
 (ns peeranoia.core
-  (:use compojure.core
-        hiccup.core
-        hiccup.page-helpers
-        hiccup.middleware
-        ring.adapter.jetty
-        ring.middleware.cookies)
+  (:use [compojure.core :only [defroutes GET]]
+        [hiccup.core :only [escape-html html]]
+        [hiccup.page-helpers :only [include-css include-js html5]]
+        [ring.adapter.jetty :only [run-jetty]]
+        [ring.middleware.cookies :only [wrap-cookies]])
   (:require [compojure.route :as route]
             [compojure.handler :as handler]))
+
+(defn- render-page
+  [{:keys [visits referer ip user-agent headers]}]
+  (html5
+   [:head
+    [:title "peeranoia"]
+    (include-css "/css/site.css")]
+   [:body
+    [:div#content
+     [:h2 "peeranoia"]
+     [:div.infoblock "Your IP is: "
+      [:span ip]]
+     [:div.infoblock "Your User-Agent is: "
+      [:span user-agent]]
+     [:div.infoblock "Your cookies report that you have been here: "
+      [:span (str visits " times")]]
+     (when-not (nil? referer)
+       [:div.infoblock "You were referred here by: "
+        [:span referer]])
+     [:p]
+     [:div.infoblock "Full headers: "
+      [:span (escape-html headers)]]]
+    (include-js "/js/g.js")]))
 
 (defn home-page
   ([r]
      (let [visits (-> r :cookies (get "value") :value Integer/parseInt inc str)
-           referer (get (:headers r) "referer")]
+           referer (get (:headers r) "referer")
+           ip (get (:headers r) "x-real-ip")
+           user-agent (get (:headers r) "user-agent")
+           headers (:headers r)]
      {:cookies {:value visits,
                 :path "/",
                 :domain "peeranoia.com" }
       :body
-      (html5
-       [:head
-        [:title "peeranoia"]
-        (include-css "/css/site.css")]
-       [:body
-        [:div#content
-         [:h2 "peeranoia"]
-         [:div.infoblock "Your IP is: "
-          [:label (get (:headers r) "x-real-ip")]]
-         [:div.infoblock "Your User-Agent is: "
-          [:label (get (:headers r) "user-agent")]]
-         [:div.infoblock "Your cookies report that you have been here: "
-          [:label (str visits " times")]]
-         (when-not (nil? referer)
-           [:div.infoblock "You were referred here by: "
-            [:label referer]])
-         [:p]
-         [:div.infoblock "Full headers: "
-          [:label (escape-html (:headers r))]]]
-        (include-js "/js/g.js")])})))
+      (render-page {:visits visits :referer referer :ip ip :user-agent user-agent :headers headers})})))
+
 
 (defroutes main-routes
   (GET "/" [:as r] (home-page r))
